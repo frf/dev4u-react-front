fb6f55fb1447d7d0362f0bc735e3123ad21ce3c9
Fabio Rocha <fabio@fabiofarias.com.br>
2020-10-25 23:44:00 +0000

    Card button

--- src/components/CardDev/index.tsx
+++ src/components/CardDev/index.tsx
@@ -9,17 +9,15 @@ interface CardDdevProps {
 const CardDev: React.FC<CardDdevProps> = (props)  => {
   return (
     <>
-      <div className="band">
-        <div className="item-1">
-          <Link to={"https://design.tutsplus.com/articles/international-artist-feature-malaysia--cms-26852"} className="card">
-            <div className="thumb " id="img1"></div>
-            <article>
-              <h1>International Artist Feature: Malaysia</h1>
-              <span>Mary Winkler</span>
-            </article>
-          </Link>
-        </div>
-      </div>
+     
+        <Link to={"https://design.tutsplus.com/articles/international-artist-feature-malaysia--cms-26852"} className="card">
+        <div className="thumb " id="img1"></div>
+        <article>
+            <h1>International Artist Feature: Malaysia</h1>
+            <span>Mary Winkler</span>
+        </article>
+        </Link>
+    
     </>
   );
 }
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import CardDev from '../../components/CardDev';
+import Button from '../../components/Button';
 import './styles.css';
 
 function Search() {
   return (
     <>
-        <CardDev />
-        <CardDev />
-        <CardDev />
+            <ul className="flex-container nowrap">
+                <li className="flex-item">
+                    <Button buttonSize='btn--wide' buttonColor='blue'>
+                    PHP
+                    </Button>
+                </li>
+                <li className="flex-item">
+                    <Button buttonSize='btn--wide' buttonColor='blue'>
+                        React
+                    </Button>
+                </li>
+                <li className="flex-item">
+                    <Button buttonSize='btn--wide' buttonColor='blue'>
+                    Vue
+                    </Button>
+                </li>
+            </ul>
+
+
+            <div className="flex-container wrap">
+                 <div className="row">
+                    <div className="column"><CardDev /></div>
+                    <div className="column"><CardDev /></div>
+                    <div className="column"><CardDev /></div>
+                    <div className="column"><CardDev /></div>
+                    <div className="column"><CardDev /></div>
+                    <div className="column"><CardDev /></div>
+                </div>
+                
+            </div>
+
     </>
   );
 }
